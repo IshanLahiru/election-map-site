4329a56775da28b0a31e54663573799e24ef10f8
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-07 04:09:04 +0530

    feat: Add data interfaces for Division, Party, and DataContextType

---
+++ src/contexts/data-context/types/data-intarfaces.tsx
@@ -0,0 +1,50 @@
+export interface Division {
+  divisionId: string;
+  divisionName: string;
+}
+
+export interface Party {
+  partyCode: string;
+  partyName: string;
+  candidate: string;
+  image: string;
+  symbol: string;
+  color: string;
+}
+
+export interface DataContextType {
+  divisionArray: Division[];
+  partyArray: Party[];
+  resultArray: ElectionResult[];
+}
+
+interface PartyResult {
+  party_code: string;
+  votes: number;
+  percentage: string;
+  party_name: string;
+  candidate: string;
+}
+
+interface Summary {
+  valid: number;
+  rejected: number;
+  polled: number;
+  electors: number;
+  percent_valid: string;
+  percent_rejected: string;
+  percent_polled: string;
+}
+
+export interface ElectionResult {
+  timestamp: string;
+  level: string;
+  ed_code: string;
+  ed_name: string;
+  pd_code: string;
+  pd_name: string;
+  by_party: PartyResult[];
+  summary: Summary;
+  type: string;
+  sequence_number: string;
+}
