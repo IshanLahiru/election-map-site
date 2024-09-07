77a0fcd37cbf3e757e60d9e075490448f3d66854
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-07 06:40:54 +0530

    refactor: Remove unused code and improve readability in MapComponent

--- src/components/map-component/map-component.tsx
+++ src/components/map-component/map-component.tsx
@@ -1,33 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import DivisionSvg from './maps/map-divisions.svg';
-import DistrictSvg from './maps/map-districts.svg';
-import { parse, stringify } from 'svgson';
+import React, { useState } from 'react';
 
-import { districtMap, divisionMap, pathExample } from './maps/dmap';
 import './style.css';
 import MapDistricts from './maps/map-districts';
 import MapDivision from './maps/map-divisions';
 
 const MapContent: React.FC = () => {
   const [districtsDivisionToggler, setDistrictsDivisionToggler] = useState(true);
-  const [divisionSvg, setDivisionSvg] = useState(DivisionSvg);
-  const [districtSvg, setDistrictSvg] = useState(DistrictSvg);
+
   const [hoverInfo, setHoverInfo] = useState({ visible: false, x: 0, y: 0, content: '' });
-
-  useEffect(() => {
-    setDistrictSvg(DistrictSvg);
-    setDivisionSvg(DivisionSvg);
-
-    parse(districtMap).then((result) => {
-      console.log('District SVG parsed:', result);
-    });
-    parse(divisionMap).then((result) => {
-      console.log('Division SVG parsed:', result);
-    });
-    parse(pathExample).then((result) => {
-      console.log('District SVG parsed:', result);
-    });
-  }, []);
 
   // Handle button click to toggle between district and division SVG
   const handleToggle = (type: 'district' | 'division') => {
