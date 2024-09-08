a184602439798b6a05f17f9b60255c92a7ec3a63
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-08 13:34:15 +0530

    refactor: update App component to use DataProvider for context

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import MapContent from './components/map-component/map-component';
 import Sidebar from './components/sidebar/sidebar';
 import BottomSection from './components/bottom-section/bottom-section';
+import { DataProvider } from './contexts/data-context/data-context';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div>
-      <div className="main-container">
-        <MapContent/>
-        <Sidebar />
-        <div id="bottom-section-container" className="bottom-section">
-          <BottomSection resultArray={[]} />
+      <DataProvider>
+        <div className="main-container">
+          <MapContent />
+          <Sidebar />
+          <div id="bottom-section-container" className="bottom-section">
+            <BottomSection resultArray={[]} />
+          </div>
         </div>
-      </div>
+      </DataProvider>
     </div>
   );
 };
