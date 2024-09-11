dfd0a0b4ea62d095da5844f2259dcd76bf910f54
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-11 10:42:01 +0530

    Add ModalCard component for displaying party information in a futuristic card style

---
+++ src/components/cards/modal-card/modal-card.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './style.css';
+
+interface ModalCardProps {
+  ob: {
+    iconUrl: string;
+    name: string;
+    voteCount: number;
+    percentage: number;
+    rank: number;
+  };
+}
+
+const ModalCard: React.FC<ModalCardProps> = ({ ob }) => {
+  return (
+    <div className="card futuristic-card">
+      <div className="party-logo">
+        <img src={ob.iconUrl} alt={`1`} />
+      </div>
+      <div className="dtlSection">
+        <div className='party-name'>{ob.name}</div>
+        <div className='progressbarPart'>
+            <div className='progress-bar'>
+            <div className="progress-bar-fill" style={{ width: `${ob.percentage}%` }}></div>
+            </div>
+        </div>
+        <div className='percentage-and-count'>
+            <div className='vote-count'>{ob.voteCount.toLocaleString()}</div>
+            <div className='percentage'>{ob.percentage}%</div>
+        </div>
+
+      </div>
+      <div className="rank-number">#{ob.rank}</div>
+    </div>
+  );
+};
+
+export default ModalCard;
