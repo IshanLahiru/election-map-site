324ce939731defee74275c888a2c8d887f8da8e0
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-11 19:13:10 +0530

    Refactor ModalCard component to include color property in party logo and progress bar

--- src/components/cards/modal-card/modal-card.tsx
+++ src/components/cards/modal-card/modal-card.tsx
@@ -8,6 +8,7 @@ interface ModalCardProps {
     voteCount: number;
     percentage: number;
     rank: number;
+    color: string;
   };
 }
 
@@ -15,22 +16,20 @@ const ModalCard: React.FC<ModalCardProps> = ({ ob }) => {
   return (
     <div className="card futuristic-card">
       <div className="party-logo">
-        <img src={ob.iconUrl} alt={`1`} />
+        <img src={ob.iconUrl} alt={`1`} style={{backgroundColor: `${ob.color}` }} />
       </div>
       <div className="dtlSection">
         <div className='party-name'>{ob.name}</div>
         <div className='progressbarPart'>
             <div className='progress-bar'>
-            <div className="progress-bar-fill" style={{ width: `${ob.percentage}%` }}></div>
+            <div className="progress-bar-fill" style={{ width: `${ob.percentage}%`, backgroundColor: `${ob.color}`   }}></div>
             </div>
         </div>
         <div className='percentage-and-count'>
             <div className='vote-count'>{ob.voteCount.toLocaleString()}</div>
             <div className='percentage'>{ob.percentage}%</div>
         </div>
-
       </div>
-      <div className="rank-number">#{ob.rank}</div>
     </div>
   );
 };
