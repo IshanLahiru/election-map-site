da1e8fc8c8353589544393324dddc9e5e44f302d
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-11 19:15:40 +0530

    Refactor ModalCard component to include color property in party logo and progress bar

--- src/components/result-modal/result-modal.tsx
+++ src/components/result-modal/result-modal.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { FullData } from '../map-component/map-component';
 import './style.css';
 import ModalCard from '../cards/modal-card/modal-card';
+import { DataContext } from '../../contexts/data-context/data-context';
+import { ElectionResult, Party } from '../../contexts/data-context/types/data-intarfaces';
+import { getPartyByName } from '../../contexts/data-context/util/data-util';
 
 interface ModalProps {
   isVisible: boolean;
   onClose: () => void;
-  dta: FullData;
+  dta: ElectionResult;
   title: string;
 }
 
 const Modal: React.FC<ModalProps> = ({ isVisible, onClose, dta, title }) => {
+  const {
+    partyArray
+  } = useContext(DataContext)|| {
+    divisionArray: [],
+    provinceArray: [],
+    partyArray: [],
+    divisionColorArray: [],
+    provinceColorArray: [],
+  };
+
+  const [allIslandResultByParty,setAllIslandResultByPArty] = useState<PartyResult[]>([]);
+  const [parties,setParties] = useState<Party[]>([]);
+
+  useEffect(() => {
+    if (dta) {
+      setAllIslandResultByPArty(dta.by_party);
+    }
+  }, [dta]);
+
+  useEffect(() => {
+    setParties(partyArray);
+  }, [partyArray]);
+
   if (!isVisible) return null;
 
   return (
@@ -25,18 +51,18 @@ const Modal: React.FC<ModalProps> = ({ isVisible, onClose, dta, title }) => {
           </button>
         </div>
         <div className="modal-content">
-          {dta.island_wide.length > 0 ? (
-            dta.island_wide.map((party, index) => (
+          {dta.by_party.length > 0 ? (
+            dta.by_party.map((party, index) => (
               <ModalCard
-                key={index}
-                ob={{
-                  iconUrl: '',
-                  name: party.party_name,
-                  voteCount: party.votes,
-                  percentage: party.percentage,
-                  rank: party.seats || 0,
-                }}
-              />
+              key={index}
+              ob={{
+                iconUrl: `src/contexts/data-context/data/svgs/${getPartyByName(parties, party.party_name)?.image}`,
+                name: party.party_name,
+                voteCount: party.votes,
+                percentage: (isNaN(+party.percentage.replace('%', '').trim()) ? 0 : +party.percentage.replace('%', '').trim()),
+                rank: index+1,
+                color: getPartyByName(parties, party.party_name)?.color || '#0000',
+              }}/>
             ))
           ) : (
             <p>No data available</p>
